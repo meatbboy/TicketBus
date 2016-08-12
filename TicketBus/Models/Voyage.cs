46ab5baaa617d315ef6943f1ceec0ffe334f75fa
meatbboy <shvaps.electro@gmail.com>
2016-08-13 00:30:09 +0300

    Add BusStops, Voyage models

---
+++ TicketBus/Models/Voyage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TicketBus.Models
+{
+    public class Voyage
+    {
+        public int Id { get; set; }
+
+        public int DepartureBusStopId { get; set; }
+
+        public int ArrivalBusStopId { get; set; }
+
+        public DateTime DepartureDateTime { get; set; }
+
+        public DateTime ArrivalDateTime { get; set; }
+       
+        [Column(TypeName = "time")]
+        public TimeSpan TravelTime { get; set; }
+
+        public int VoyageNumber { get; set; }
+
+        public string VoyageName { get; set; }
+
+        public int NumberOfSeats { get; set; }
+
+        public int OneTicketCost { get; set; }
+    }
+}
