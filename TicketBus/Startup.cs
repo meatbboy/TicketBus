5a40611ed28f75c0fc2dca46ac8986d2e580ca1a
meatbboy <shvaps.electro@gmail.com>
2016-08-12 20:11:11 +0300

    Add project files.

---
+++ TicketBus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicketBus.Startup))]
+namespace TicketBus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
