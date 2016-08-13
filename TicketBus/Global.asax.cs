0e600e16f2c3f1db8d3ee4293730ba1e0a53125d
meatbboy <shvaps.electro@gmail.com>
2016-08-13 23:21:46 +0300

    Modificate User: Add FirstName, LastName. Displaying FullName in navbar.

--- TicketBus/Global.asax.cs
+++ TicketBus/Global.asax.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using TicketBus.Models;
 
 namespace TicketBus
 {
---
+++ TicketBus/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace TicketBus.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TicketBus.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "TicketBus.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(TicketBus.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
